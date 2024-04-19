b378d134c0497909e4aeafd36345526a58b7262e
Boo_oyo <136545068+Boooyo@users.noreply.github.com>
2024-04-19 09:55:29 +0900

    Add Refactoring Code

--- 02.interface/draw_line.py
+++ 02.interface/draw_line.py
@@ -1,30 +1,33 @@
 import cv2
 
+def draw_lines(img, points, color, thickness=1, line_type=cv2.LINE_8):
+    for i in range(len(points) - 1):
+        cv2.line(img, points[i], points[i+1], color, thickness, line_type)
+
+def draw_diagonal(img, color, thickness=1):
+    cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), color, thickness)
+
 img = cv2.imread('../img/blank_500.jpg')
 
-cv2.line(img, (50, 50), (150, 50), (255,0,0))   # 파란색 1픽셀 선
-cv2.line(img, (200, 50), (300, 50), (0,255,0))  # 초록색 1픽셀 선
-cv2.line(img, (350, 50), (450, 50), (0,0,255))  # 빨간색 1픽셀 선
+# 단일 색상 선 그리기
+draw_lines(img, [(50, 50), (150, 50)], (255, 0, 0))
+draw_lines(img, [(200, 50), (300, 50)], (0, 255, 0))
+draw_lines(img, [(350, 50), (450, 50)], (0, 0, 255))
 
-# 하늘색(파랑+초록) 10픽셀 선      
-cv2.line(img, (100, 100), (400, 100), (255,255,0), 10)          
-# 분홍(파랑+빨강) 10픽셀 선      
-cv2.line(img, (100, 150), (400, 150), (255,0,255), 10)          
-# 노랑(초록+빨강) 10픽셀 선      
-cv2.line(img, (100, 200), (400, 200), (0,255,255), 10)          
-# 회색(파랑+초록+빨강) 10픽셀 선  
-cv2.line(img, (100, 250), (400, 250), (200,200,200), 10)        
-# 검정 10픽셀 선    
-cv2.line(img, (100, 300), (400, 300), (0,0,0), 10)                    
+# 혼합 색상 선 그리기
+draw_lines(img, [(100, 100), (400, 100)], (255, 255, 0), 10)
+draw_lines(img, [(100, 150), (400, 150)], (255, 0, 255), 10)
+draw_lines(img, [(100, 200), (400, 200)], (0, 255, 255), 10)
+draw_lines(img, [(100, 250), (400, 250)], (200, 200, 200), 10)
+draw_lines(img, [(100, 300), (400, 300)], (0, 0, 0), 10)
 
-# 4연결 선
-cv2.line(img, (100, 350), (400, 400), (0,0,255), 20, cv2.LINE_4)   
-# 8연결 선
-cv2.line(img, (100, 400), (400, 450), (0,0,255), 20, cv2.LINE_8)    
-# 안티에일리어싱 선 
-cv2.line(img, (100, 450), (400, 500), (0,0,255), 20, cv2.LINE_AA)   
-# 이미지 전체에 대각선 
-cv2.line(img, (0,0), (500,500), (0,0,255))                      
+# 다양한 연결 유형 선 그리기
+draw_lines(img, [(100, 350), (400, 400)], (0, 0, 255), 20, cv2.LINE_4)
+draw_lines(img, [(100, 400), (400, 450)], (0, 0, 255), 20, cv2.LINE_8)
+draw_lines(img, [(100, 450), (400, 500)], (0, 0, 255), 20, cv2.LINE_AA)
+
+# 이미지 전체에 대각선 그리기
+draw_diagonal(img, (0, 0, 255))
 
 cv2.imshow('lines', img)
 cv2.waitKey(0)
