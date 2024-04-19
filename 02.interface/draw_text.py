c10cd2b01fe924ab0e04007228bef0bf27e470b6
Boooyo <136545068+Boooyo@users.noreply.github.com>
2024-04-19 10:54:47 +0900

    Add Refactoring Code

--- 02.interface/draw_text.py
+++ 02.interface/draw_text.py
@@ -1,40 +1,34 @@
 import cv2
 
-img = cv2.imread('../img/blank_500.jpg')
+def draw_texts(img, texts):
+    for text in texts:
+        cv2.putText(img, text[0], text[1], text[2], text[3], text[4])
 
-# sans-serif small
-cv2.putText(img, "Plain", (50, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0,0))            
-# sans-serif normal
-cv2.putText(img, "Simplex", (50, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0,0))        
-# sans-serif bold
-cv2.putText(img, "Duplex", (50, 110), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0,0))         
-# sans-serif normall X2  ---①
-cv2.putText(img, "Simplex", (200, 110), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,250)) 
+def main():
+    img = cv2.imread('../img/blank_500.jpg')
 
-# serif small
-cv2.putText(img, "Complex Small", (50, 180), cv2.FONT_HERSHEY_COMPLEX_SMALL, \
-            1, (0, 0,0))   
-# serif normal
-cv2.putText(img, "Complex", (50, 220), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0,0))
-# serif bold
-cv2.putText(img, "Triplex", (50, 260), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0,0))               
-# serif normal X2  ---②
-cv2.putText(img, "Complex", (200, 260), cv2.FONT_HERSHEY_TRIPLEX, 2, (0,0,255))               
+    # 텍스트와 속성 배열로 정의
+    texts = [
+        ("Plain", (50, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0,0)),                  # sans-serif small
+        ("Simplex", (50, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0,0)),              # sans-serif normal
+        ("Duplex", (50, 110), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0,0)),               # sans-serif bold
+        ("Simplex", (200, 110), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,250)),           # sans-serif normal X2
+        ("Complex Small", (50, 180), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0,0)), # serif small
+        ("Complex", (50, 220), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0,0)),              # serif normal
+        ("Triplex", (50, 260), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0,0)),              # serif bold
+        ("Complex", (200, 260), cv2.FONT_HERSHEY_TRIPLEX, 2, (0,0,255)),            # serif normal X2
+        ("Script Simplex", (50, 330), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 0,0)),# hand-wringing sans-serif
+        ("Script Complex", (50, 370), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (0, 0,0)),# hand-wringing serif
+        ("Plain Italic", (50, 430), cv2.FONT_HERSHEY_PLAIN | cv2.FONT_ITALIC, 1, (0, 0,0)), # sans-serif + italic
+        ("Complex Italic", (50, 470), cv2.FONT_HERSHEY_COMPLEX | cv2.FONT_ITALIC, 1, (0, 0,0)) # sarif + italic
+    ]
 
-# hand-wringing sans-serif
-cv2.putText(img, "Script Simplex", (50, 330), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, \
-            1, (0, 0,0)) 
-# hand-wringing serif
-cv2.putText(img, "Script Complex", (50, 370), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, \
-            1, (0, 0,0)) 
+    # 텍스트 그리기
+    draw_texts(img, texts)
 
-# sans-serif + italic ---③
-cv2.putText(img, "Plain Italic", (50, 430), \
-            cv2.FONT_HERSHEY_PLAIN | cv2.FONT_ITALIC, 1, (0, 0,0)) 
-# sarif + italic
-cv2.putText(img, "Complex Italic", (50, 470), \
-            cv2.FONT_HERSHEY_COMPLEX | cv2.FONT_ITALIC, 1, (0, 0,0)) 
+    cv2.imshow('draw text', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.imshow('draw text', img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
