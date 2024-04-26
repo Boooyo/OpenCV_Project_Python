9fa0703fa94894e51a1feb7e3bdff22a43ef14f6
Boooyo <136545068+Boooyo@users.noreply.github.com>
2024-04-26 14:40:49 +0900

    Add Refactoring Code

--- 02.interface/event_mouse_circle.py
+++ 02.interface/event_mouse_circle.py
@@ -1,18 +1,22 @@
 import cv2
 
-title = 'mouse event'                   # 창 제목
-img = cv2.imread('../img/blank_500.jpg') # 백색 이미지 읽기
-cv2.imshow(title, img)                  # 백색 이미지 표시
+def draw_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(image, (x, y), 30, (0, 0, 0), -1)
+        cv2.imshow(window_title, image)
 
-def onMouse(event, x, y, flags, param): # 아무스 콜백 함수 구현 ---①
-    print(event, x, y, )                # 파라미터 출력
-    if event == cv2.EVENT_LBUTTONDOWN:  # 왼쪽 버튼 누름인 경우 ---②
-        cv2.circle(img, (x,y), 30, (0,0,0), -1) # 지름 30 크기의 검은색 원을 해당 좌표에 그림
-        cv2.imshow(title, img)          # 그려진 이미지를 다시 표시 ---③
+def main():
+    window_title = 'Mouse Event'
+    image = cv2.imread('../img/blank_500.jpg')
+    cv2.imshow(window_title, image)
+    cv2.setMouseCallback(window_title, draw_circle)
+    
+    while True:
+        key = cv2.waitKey(0)
+        if key == 27:  # ESC 키를 누르면 종료
+            break
+    
+    cv2.destroyAllWindows()
 
-cv2.setMouseCallback(title, onMouse)    # 마우스 콜백 함수를 GUI 윈도우에 등록 ---④
-
-while True:
-    if cv2.waitKey(0) & 0xFF == 27:     # esc로 종료
-        break
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
