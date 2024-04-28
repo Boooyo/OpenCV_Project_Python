504cde16b0f1cc8923933617d06b131ff9ec9863
Boooyo <136545068+Boooyo@users.noreply.github.com>
2024-04-28 16:20:37 +0900

    Add Refactoring Code

--- 02.interface/img_show.py
+++ 02.interface/img_show.py
@@ -1,16 +1,19 @@
-'''
-GitHub : https://github.com/dltpdn/book_opencv_prject_using_python
-Author : Lee Sewoo(이세우, dltpdn@gmail.com)
-'''
 import cv2
 
-img_file = "../img/yeosu.jpg" # 표시할 이미지 경로            ---①
-img = cv2.imread(img_file)  # 이미지를 읽어서 img 변수에 할당 ---②
+class ImageViewer:
+    def __init__(self, img_file):
+        self.img_file = img_file
 
-if img is not None:
-  cv2.imshow('IMG', img)   # 읽은 이미지를 화면에 표시      --- ③
-  cv2.waitKey()           # 키가 입력될 때 까지 대기      --- ④
-  cv2.destroyAllWindows()  # 창 모두 닫기            --- ⑤
-else:
-    print('No image file.')
-    
+    def show_image(self):
+        img = cv2.imread(self.img_file)
+        if img is not None:
+            cv2.imshow('Image Viewer', img)
+            cv2.waitKey()
+            cv2.destroyAllWindows()
+        else:
+            print('No image file.')
+
+if __name__ == "__main__":
+    img_file = "../img/yeosu.jpg"
+    image_viewer = ImageViewer(img_file)
+    image_viewer.show_image()
