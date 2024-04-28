9a8a3baf538e14c1c8b7e2e557b44637c71619a2
Boooyo <136545068+Boooyo@users.noreply.github.com>
2024-04-28 16:19:55 +0900

    Add Refactoring Code

--- 02.interface/img_show_gray.py
+++ 02.interface/img_show_gray.py
@@ -1,12 +1,14 @@
 import cv2
 
-img_file = "../img/yeosu.jpg" 
-img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)  #그레이 스케일로 읽기
+def show_grayscale_image(img_file):
+    img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
+    if img is not None:
+        cv2.imshow('Grayscale Image', img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+    else:
+        print('No image file.')
 
-if img is not None:
-    cv2.imshow('IMG', img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-else:
-    print('No image file.')
-    
+if __name__ == "__main__":
+    img_file = "../img/yeosu.jpg"
+    show_grayscale_image(img_file)
