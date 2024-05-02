674d342793b330405d973dc5356c8f5cbaaf10ce
Boooyo <136545068+Boooyo@users.noreply.github.com>
2024-05-02 11:02:00 +0900

    Add Refactoring Code

--- 09.ml/k-means_random.py
+++ 09.ml/k-means_random.py
@@ -1,24 +1,33 @@
-import numpy as np, cv2
+import numpy as np
+import cv2
 import matplotlib.pyplot as plt
 
-# 0~150 임의의 2수, 25개 ---①
-a = np.random.randint(0,150,(25,2))
-# 128~255 임의의 2수, 25개  ---②
-b = np.random.randint(128, 255,(25,2))
-# a, b를 병합 ---③
-data = np.vstack((a,b)).astype(np.float32)
-# 중지 요건 ---④
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-# 평균 클러스터링 적용 ---⑤
-ret,label,center=cv2.kmeans(data,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-# label에 따라 결과 분류 ---⑥
-red = data[label.ravel()==0]
-blue = data[label.ravel()==1]
+def generate_random_data(low, high, size):
+    return np.random.randint(low, high, size)
 
-# plot에 결과 출력 ---⑦
-plt.scatter(red[:,0],red[:,1], c='r')
-plt.scatter(blue[:,0],blue[:,1], c='b')
-# 각 그룹의 중앙점 출력 ---⑧
-plt.scatter(center[0,0],center[0,1], s=100, c='r', marker='s')
-plt.scatter(center[1,0],center[1,1], s=100, c='b', marker='s')
-plt.show()
+def plot_clusters(data, label, center):
+    colors = ['r', 'b']
+    markers = ['s', 'o'] # 정사각형과 원 표시
+    for i in range(len(colors)):
+        cluster_data = data[label.ravel() == i]
+        plt.scatter(cluster_data[:, 0], cluster_data[:, 1], c=colors[i])
+        plt.scatter(center[i, 0], center[i, 1], s=100, c=colors[i], marker=markers[i])
+    plt.show()
+
+def main():
+    # 랜덤 데이터 생성
+    a = generate_random_data(0, 150, (25, 2))
+    b = generate_random_data(128, 255, (25, 2))
+    data = np.vstack((a, b)).astype(np.float32)
+
+    # 중지 조건 정의
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
+    # k-means 클러스터링 적용
+    ret, label, center = cv2.kmeans(data, 2, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    # 클러스터링 결과 플롯
+    plot_clusters(data, label, center)
+
+if __name__ == "__main__":
+    main()
