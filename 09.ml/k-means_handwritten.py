70a89fda4b2e6153da6b092dad2cfde1ff196d88
Boooyo <136545068+Boooyo@users.noreply.github.com>
2024-05-29 10:06:21 +0900

    코드 수정

--- 09.ml/k-means_handwritten.py
+++ 09.ml/k-means_handwritten.py
@@ -1,18 +1,25 @@
-import cv2, numpy as np
+import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 import mnist
 
-# 공통 모듈로 부터 MINST 전체 이미지 데이타 읽기 ---①
+# MNIST 데이터 로드
 data, _ = mnist.getData()
-# 중지 요건 
+
+# k-means 클러스터링의 중지 조건 설정 (10회 반복 또는 1.0 이하의 정확도)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-# 평균 클러스터링 적용, 10개의 그룹으로 묶음 ---②
-ret,label,center=cv2.kmeans(data,10,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-# 중앙점 이미지 출력
+
+# k-means 클러스터링 적용, 10개의 클러스터로 분할
+ret, labels, centers = cv2.kmeans(data, 10, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+# 각 클러스터의 중심을 이미지로 변환하여 출력
+plt.figure(figsize=(10, 5))
 for i in range(10):
-    # 각 중앙점 값으로 이미지 생성 ---③
-    cent_img = center[i].reshape(20,20).astype(np.uint8)
-    plt.subplot(2,5, i+1)
-    plt.imshow(cent_img, 'gray')
-    plt.xticks([]);plt.yticks([])
+    # 각 클러스터 중심을 20x20 이미지로 변환
+    center_img = centers[i].reshape(20, 20).astype(np.uint8)
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(center_img, cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+
 plt.show()
