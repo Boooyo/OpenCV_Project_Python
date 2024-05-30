f5961dce9e62fcb0252f9b5a14a89f30de2d2987
Boooyo <136545068+Boooyo@users.noreply.github.com>
2024-05-30 16:57:34 +0900

    코드 수정

--- 09.ml/k-means_color.py
+++ 09.ml/k-means_color.py
@@ -2,6 +2,14 @@ import numpy as np
 import cv2
 
 def kmeans_color_quantization(image_path, K=16):
+    """
+    이미지 파일 경로와 K값을 입력받아 색상 양자화를 수행하고,
+    양자화된 이미지를 반환하는 함수.
+    
+    :param image_path: 색상 양자화를 수행할 이미지 파일의 경로
+    :param K: K-means 클러스터링에서 사용할 군집의 수
+    :return: 양자화된 이미지
+    """
     # 이미지 읽기
     img = cv2.imread(image_path)
     
@@ -23,13 +31,19 @@ def kmeans_color_quantization(image_path, K=16):
     # 원본 이미지 형태로 변환
     quantized = quantized.reshape(img.shape)
     
-    # 원본 이미지와 군집화된 이미지를 수평으로 결합하여 출력
-    merged = np.hstack((img, quantized))
-    
-    # 결과 출력
-    cv2.imshow('KMeans Color Quantization', merged)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    return quantized
 
-# 함수 호출하여 이미지에 대한 색상 양자화 수행
-kmeans_color_quantization('../img/taekwonv1.jpg', K=16)
+# 함수 호출하여 이미지에 대한 색상 양자화 수행 및 결과 저장
+image_path = '../img/taekwonv1.jpg'
+quantized_image = kmeans_color_quantization(image_path, K=16)
+
+# 원본 이미지 읽기
+original_image = cv2.imread(image_path)
+
+# 원본 이미지와 양자화된 이미지를 수평으로 결합
+merged_image = np.hstack((original_image, quantized_image))
+
+# 결합된 이미지 출력
+cv2.imshow('KMeans Color Quantization', merged_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
